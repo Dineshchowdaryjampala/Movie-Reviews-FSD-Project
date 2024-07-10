19f88c5ad0f4e712333c7f808417a89de6c60b74
Nallamilli Shanmuka Leela Karthikeya Reddy <134484706+NSL-Karthikeya-Reddy@users.noreply.github.com>
2024-07-11 00:03:33 +0530

    Update private.client.js

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://movie-review-system-server.vercel.app/";
+const baseURL = "https://movie-review-system-server.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
