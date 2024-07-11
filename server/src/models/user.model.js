07b5023c15fa7269bfb41f791f2d0f885c87ab92
Dineshchowdaryjampala <154117040+Dineshchowdaryjampala@users.noreply.github.com>
2024-07-11 10:30:53 +0530

    Update user.model.js

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import modelOptions from "./model.options.js";
 import crypto from "crypto";
-
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
